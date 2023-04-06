4d290959a9fcebdc492ff2511a8a1d2dc5a97e63
Tszfai Choy <tszfai04@gmail.com>
2023-04-06 14:43:55 -0400

    functional animation + image

--- js/buttons.js
+++ js/buttons.js
@@ -1,12 +1,15 @@
 let colorImage = document.getElementById("first");
 let button1 = document.getElementById("button1");
-
-function changeMono() {
+colorImage.setAttribute("src", "../images/first.jpg");
+async function changeMono() {
+  colorImage.style.opacity = 0;
+  await new Promise((r) => setTimeout(r, 1000));
   if (colorImage.getAttribute("src") === "../images/first.jpg") {
     colorImage.setAttribute("src", "../images/download.png");
   } else {
     colorImage.setAttribute("src", "../images/first.jpg");
   }
+  colorImage.style.opacity = 1;
 }
 
 button1.addEventListener("click", changeMono);
