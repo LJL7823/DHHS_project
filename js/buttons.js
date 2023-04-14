e92d9ab90fbf378a03459b11d86ec900f06b2b1b
Tszfai Choy <tszfai04@gmail.com>
2023-04-14 07:38:58 -0400

    updates

--- js/buttons.js
+++ js/buttons.js
@@ -1,15 +1,35 @@
-let colorImage = document.getElementById("first");
+let colorImage = document.getElementById("image");
 let button1 = document.getElementById("button1");
-colorImage.setAttribute("src", "../images/first.jpg");
-async function changeMono() {
-  colorImage.style.opacity = 0;
-  await new Promise((r) => setTimeout(r, 1000));
-  if (colorImage.getAttribute("src") === "../images/first.jpg") {
-    colorImage.setAttribute("src", "../images/download.png");
-  } else {
-    colorImage.setAttribute("src", "../images/first.jpg");
-  }
-  colorImage.style.opacity = 1;
-}
 
-button1.addEventListener("click", changeMono);
+const before = [
+  "../images/first.jpg",
+  "../images/first.jpg",
+  "../images/first.jpg",
+  "../images/first.jpg",
+  "../images/first.jpg",
+];
+const after = [
+  "../images/download.png",
+  "../images/download.png",
+  "../images/download.png",
+  "../images/download.png",
+  "../images/download.png",
+];
+
+for (let x = 1; x <= 4; x++) {
+  let button = document.getElementById(`button${x}`);
+  let colorImage = document.getElementById(`image${x}`);
+  console.log(`image${x}`);
+  colorImage.setAttribute("src", before[x]);
+  async function onClickAnimation() {
+    colorImage.style.opacity = 0;
+    await new Promise((r) => setTimeout(r, 1000));
+    if (colorImage.getAttribute("src") === before[x - 1]) {
+      colorImage.setAttribute("src", after[x - 1]);
+    } else {
+      colorImage.setAttribute("src", before[x - 1]);
+    }
+    colorImage.style.opacity = 1;
+  }
+  button.addEventListener("click", onClickAnimation);
+}
