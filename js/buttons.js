0ac5e131f733686ca9055e8bf7610db16950f9f9
Tszfai Choy <tszfai04@gmail.com>
2023-04-25 21:56:54 -0400

    basically finished

--- js/buttons.js
+++ js/buttons.js
@@ -1,26 +1,39 @@
 let colorImage = document.getElementById("image");
-let button1 = document.getElementById("button1");
 
 const before = [
   "images/colorblind_images_buttons/standard.png",
   "images/colorblind_images_buttons/standard.png",
   "images/colorblind_images_buttons/standard.png",
   "images/colorblind_images_buttons/standard.png",
-  "images/colorblind_images_buttons/standard.png",
+  "images/graphs/graphStandard.png",
+  "images/graphs/graphStandard.png",
+  "images/graphs/graphStandard.png",
+  "images/graphs/graphStandard.png",
+  "images/model/partStandard.png",
+  "images/model/partStandard.png",
+  "images/model/partStandard.png",
+  "images/model/partStandard.png",
 ];
 const after = [
   "images/colorblind_images_buttons/deuteranomaly.png",
   "images/colorblind_images_buttons/protanopia.png",
   "images/colorblind_images_buttons/tritanopia.png",
   "images/colorblind_images_buttons/achromoplastia.png",
-  "images/colorblind_images_buttons/deuteranopia.png",
+  "images/graphs/graphDeuteranomaly.png",
+  "images/graphs/graphProtanopia.png",
+  "images/graphs/graphTritanopia.png",
+  "images/graphs/graphAchromatopsia.png",
+  "images/model/partDeuteranomaly.png",
+  "images/model/partProtanopia.png",
+  "images/model/partTritanopia.png",
+  "images/model/partAchromatopsia.png",
 ];
 
-for (let x = 1; x <= 4; x++) {
+for (let x = 1; x <= 12; x++) {
   let button = document.getElementById(`button${x}`);
   let colorImage = document.getElementById(`image${x}`);
   console.log(`image${x}`);
-  colorImage.setAttribute("src", before[x]);
+  colorImage.setAttribute("src", before[x - 1]);
   async function onClickAnimation() {
     colorImage.style.opacity = 0;
     await new Promise((r) => setTimeout(r, 1000));
